ca001c33364ab2a65e5b40afab4be7a3fa1cf5f6
Your Name01~git <saifsahai2002@gmail.com>
2025-12-06 18:54:50 +0600

    basic data is consoling

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
-import express from "express"
+import express, { Request, Response } from "express"
+
+
 const app = express()
 const port = 5000
+app.use(express.json());
+// app.use(express.urlencoded())
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World - Rizwan Wahid Mehrab Rayan Marzia!')
+})
+
+// * Post Method
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.status(201).json({
+        success: true,
+        message: "API is working",
+        body: req?.body
+    })
+    
 })
 
 app.listen(port, () => {
