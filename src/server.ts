5f109154cdc4432403d57b7b69e76be113b638b2
Your Name01~git <saifsahai2002@gmail.com>
2025-12-07 12:28:06 +0600

    deleting single data successfully

--- src/server.ts
+++ src/server.ts
@@ -109,7 +109,7 @@ app.get("/users/:id", async (req: Request, res: Response) => {
 })
 // * Update single user data
 app.put("/users/:id", async (req: Request, res: Response) => {
-    const {name, email} = req.body;
+    const { name, email } = req.body;
     try {
         const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`, [name, email, req.params.id])
         if (result.rows.length === 0) {
@@ -123,6 +123,30 @@ app.put("/users/:id", async (req: Request, res: Response) => {
                 success: true,
                 message: "User updated successfully",
                 data: result.rows[0]
+            })
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+})
+// * Delete single user data
+app.delete("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id])
+        if (result.rowCount === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: "User deleted successfully",
+                data: result.rows
             })
         }
     } catch (error: any) {
