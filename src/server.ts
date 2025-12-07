3b9089f9489f6dcc2ff2a3072349b900dae3932a
Your Name01~git <saifsahai2002@gmail.com>
2025-12-07 11:04:01 +0600

    getting single user

--- src/server.ts
+++ src/server.ts
@@ -83,6 +83,30 @@ app.get("/users", async (req: Request, res: Response) => {
         })
     }
 })
+// * GET Single user
+app.get("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id])
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: "User fetched successfully",
+                data: result.rows[0]
+            })
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
