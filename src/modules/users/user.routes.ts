7a5cafe3adb5b01feb8e352d80ce344ed7367127
Your Name01~git <saifsahai2002@gmail.com>
2025-12-08 17:16:36 +0600

    following modular pattern

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,25 +1,10 @@
 import { Request, Response, Router } from "express";
 import { pool } from "../../config/db";
+import { userControllers } from "./user.controller";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-    const { name, email } = req.body;
-    try {
-        const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email])
-        res.status(201).json({
-            success: true,
-            message: "Data inserted successfully!",
-            data: result.rows[0]
-        })
-    }
-    catch (error: any) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        })
-    }
-})
+router.post("/", userControllers.createUser)
 
 router.get("/", async (req: Request, res: Response) => {
     try {
