74358e538ea395dd31a0faeeba5352efb8c965d5
Your Name01~git <saifsahai2002@gmail.com>
2025-12-14 19:22:09 +0600

    ..

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,17 +1,25 @@
 import { NextFunction, Request, Response } from "express"
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
 // * A Higher order function receives a function as a parameter and returns a function. 
-const auth = () => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const token = req.headers.authorization;
-        if (!token) {
-            return res.status(500).json({message: "You're not allowed"})
+const auth = (...roles: string[]) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const token = req.headers.authorization;
+            if (!token) {
+                res.status(500).json({message: "You're not allowed"});
+            }
+            const decoded = jwt.verify(token as string, config.jwt_secret as string);
+            console.log({decoded});
+            req.user = decoded as JwtPayload;
+            next();
+        } catch (error: any) {
+            res.status(500).json({
+                success: false,
+                message: error.message
+            })
         }
-        const decoded = jwt.verify(token, config.jwt_secret as string);
-        console.log({decoded});
-        next();
     }
 }
 
--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.post("/", userControllers.createUser)
 
-router.get("/", logger, auth(), userControllers.getUser)
+router.get("/", logger, auth("admin"), userControllers.getUser)
 
 router.get("/:id", userControllers.getSingleUser)
 
